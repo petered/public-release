3f20c72f75983373174f84ce08f6e4efbe5204cf
Peter O'Connor <poconn4@gmail.com>
2017-06-12 15:42:17 +0200

    setup.py
    fdfd

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='export-demo',
+      author='Peter',
+      author_email='poconn4@gmail.com',
+      url='https://github.com/petered/export-demo.git',
+      long_description='A tool to help export demo code',
+      version=0,
+      packages=['export_repo'],
+      scripts=[])
