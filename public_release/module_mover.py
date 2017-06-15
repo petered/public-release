e711ba67006cdc71dc9d3b97737af72409d287f7
Peter O'Connor <peter.ed.oconnor@gmail.com>
2017-06-15 16:24:40 +0200

    fixed to allow other files from same project

--- public_release/module_mover.py
+++ public_release/module_mover.py
@@ -4,6 +4,9 @@ import inspect
 import os
 from shutil import rmtree
 import logging
+
+from artemis.general.should_be_builtins import detect_duplicates, izip_equal
+
 logging.basicConfig()
 LOGGER = logging.getLogger('export-demo')
 LOGGER.setLevel(logging.INFO)
@@ -13,16 +16,20 @@ def get_src_file(module):
     return module.__file__[:-1] if module.__file__.endswith('.pyc') else module.__file__
 
 
-def get_module_import_dict(object, just_own_package = True, remove_packages = True):
+def get_module_import_dict(object, scope = 'project', remove_packages = True):
     """
     Given some code object (or the full name of a module), find all the modules that must be imported for this object to
     run.
     :param object:
-    :param just_own_package:
-    :return:
+    :param scope: One of:
+        'package': Only collect modules from same root package (eg root package of foo.bar.baz is foo.
+        'project': Only collect modules in same project (ie, modules whose root package is in the same directoy as object's root package)
+        'all': Collect all dependent modueles (not recommended, as this will, for example include a bunch of numpy code).
+    :return: A dict<module_name: module_path>
     """
+    assert scope in ('package', 'project', 'all')
     if isinstance(object, (list, tuple)):
-        dicts, names = zip(*[get_module_import_dict(ob, just_own_package=just_own_package) for ob in object])
+        dicts, names = zip(*[get_module_import_dict(ob, scope=scope) for ob in object])
         return {k: v for d in dicts for k, v in d.iteritems()}, names
     elif isinstance(object, basestring):
         module = import_module(object)
@@ -33,21 +40,26 @@ def get_module_import_dict(object, just_own_package = True, remove_packages = Tr
     this_package = module.__name__.split('.')[0]
     LOGGER.info('Scanning Dependent Modules in {}.  This may take some time...'.format(this_package))
     finder.run_script(module_file)
-    if just_own_package:
-        modules = {name: mod for name, mod in finder.modules.iteritems() if name.split('.')[0]==this_package}
-    else:
-        modules = finder.modules
-    LOGGER.info('Scan Complete.  {} dependent modules found.'.format(len(modules)))
-    modules[module.__name__] = module  # Don't forget yourself!
-    module_name_to_module_path = {name: get_src_file(m) for name, m in modules.iteritems()}
+
+    module_files = {name: get_src_file(module) for name, module in finder.modules.iteritems() if module.__file__ is not None}
+    module_files[module.__name__] = get_src_file(module)  # Don't forget yourself!
+    # module_files =
+    if scope=='package':
+        module_files = {name: mod for name, mod in module_files.iteritems() if name.split('.')[0]==this_package}
+    elif scope=='project':
+        base_dir = os.path.dirname(os.path.dirname(module.__file__))
+        module_files = {name: mod for name, mod in module_files.iteritems() if mod.startswith(base_dir)}
+    LOGGER.info('Scan Complete.  {} dependent modules found.'.format(len(module_files)))
+    # module_name_to_module_path = {name: get_src_file(m) for name, m in modules.iteritems()}
     if remove_packages:
-        module_name_to_module_path = {name: path for name, path in module_name_to_module_path.iteritems() if not (path.endswith('__init__.py') or path.endswith('__init__.pyc'))}
-    return module_name_to_module_path, module.__name__
+        module_files = {name: path for name, path in module_files.iteritems() if not (path.endswith('__init__.py') or path.endswith('__init__.pyc'))}
+    return module_files, module.__name__
 
 
 def copy_modules_to_dir(object, destination_dir, root_package, code_subpackage=None, helper_subpackage ='helpers', clear_old_package = False):
 
-    modules, names = get_module_import_dict(object, just_own_package=True)
+    modules, names = get_module_import_dict(object, scope='project')
+
     if isinstance(names, basestring):
         names = [names]
     root_dir = os.path.join(destination_dir, root_package)
@@ -70,6 +82,10 @@ def copy_modules_to_dir(object, destination_dir, root_package, code_subpackage=N
             f.write('')
 
     old_name_to_new_name = {module_name: code_module_name+'.'+module_name.split('.')[-1] if module_name in names else helper_module_name+'.'+module_name.split('.')[-1] for module_name in modules.keys()}
+    duplicates = {k: v for (k, v), isdup in izip_equal(old_name_to_new_name.iteritems(), detect_duplicates(old_name_to_new_name.values())) if isdup}
+    if len(duplicates)>0:
+        raise Exception('There is a collision between two or more modules names: {}\n.  You need to rename these modules to have unique names.'.format(duplicates))
+
     old_name_to_new_path = {module_name: os.path.join(code_dir, os.path.split(module_file)[1]) if module_name in names else os.path.join(helper_dir, os.path.split(module_file)[1]) for module_name, module_file in modules.iteritems()}
 
     for module_name, module_path in modules.iteritems():
