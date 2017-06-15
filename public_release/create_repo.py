d3cf30283a3bc859608fd0e8a01dbcdb88bd8e14
Peter O'Connor <peter.ed.oconnor@gmail.com>
2017-06-15 14:15:22 +0200

    ui is complete and beautiful

--- public_release/create_repo.py
+++ public_release/create_repo.py
@@ -18,7 +18,8 @@ def fill_repo_with_initial_files(
         dependency_links = (),
         repo_dependencies = (),
         scripts=[],
-        commit_and_push = False,
+        github_url = None,
+        commit_and_push = True
         ):
 
     setup_file_txt = generate_setup_py_text(
@@ -32,59 +33,97 @@ def fill_repo_with_initial_files(
         scripts=scripts
         )
 
-    with open(os.path.join(destination_dir, 'setup.py'), 'w') as f:
-        f.write(setup_file_txt)
-
-    with open(os.path.join(destination_dir, 'setup.sh'), 'w') as f:
-        f.write(setup_sh_text)
-
-    with open(os.path.join(destination_dir, 'setup.cfg'), 'w') as f:
-        f.write(setup_cfg_text)
-
-    with open(os.path.join(destination_dir, '.gitignore'), 'w') as f:
-        f.write(dot_gitignore_text)
-
-    readme_file = os.path.join(destination_dir, 'README.md')
-    is_git_repo = subprocess.call('git status', shell=True)==0
-    if is_git_repo:
-        git_url = subprocess.check_output('git config --get remote.origin.url', shell=True)
-        if not os.path.exists(readme_file):
-            with current_directory(destination_dir):
-                repo_name = subprocess.check_output('basename `git rev-parse --show-toplevel`', shell=True)
-                with open(readme_file, 'w') as f:
-                    f.write(readme_template.format(name=package_name, git_url=git_url, repo_name=repo_name, ))
-
-        if commit_and_push:
-            subprocess.call('git add .', shell=True)
-            subprocess.call('git commit -m "added initial files"', shell=True)
-            subprocess.call('git push -u origin master', shell=True)
-
-    if root_package is not None:
-        try:
-            os.makedirs(os.path.join(destination_dir, root_package))
-        except OSError:
-            pass
-        with open(os.path.join(destination_dir, '__init__.py'), 'w') as f:
-            f.write('')
-
-    with open(os.path.join(destination_dir, 'requirements.txt'), 'w') as f:
-        f.write(get_requirements_text(repo_dependencies))
-
-
-def create_git_repo(local_folder, repo_url, add_all = True):
-
     try:
-        os.makedirs(local_folder)
+        os.makedirs(destination_dir)
     except OSError:
         pass
 
-    with current_directory(local_folder):
-        subprocess.call('git init', shell=True)
-        if add_all:
-            subprocess.call('git add .', shell=True)
-        subprocess.call('git commit -m "first commit"', shell=True)
-        subprocess.call('git remote add origin {}'.format(repo_url), shell=True)
-        subprocess.call('git push -u origin master', shell=True)
+    with current_directory(destination_dir):
+        with open('setup.py', 'w') as f:
+            f.write(setup_file_txt)
+
+        with open('setup.sh', 'w') as f:
+            f.write(setup_sh_text)
+
+        with open('setup.cfg', 'w') as f:
+            f.write(setup_cfg_text)
+
+        with open('.gitignore', 'w') as f:
+            f.write(dot_gitignore_text)
+
+        readme_file = 'README.md'
+
+        if root_package is not None:
+            try:
+                os.makedirs(os.path.join(destination_dir, root_package))
+            except OSError:
+                pass
+            with open(os.path.join(destination_dir, root_package, '__init__.py'), 'w') as f:
+                f.write('')
+
+        with open(os.path.join(destination_dir, 'requirements.txt'), 'w') as f:
+            f.write(get_requirements_text(repo_dependencies))
+
+        if github_url is not None:
+            is_git_repo = subprocess.call('git status', shell=True)==0
+            if is_git_repo:
+                remote_url = subprocess.check_output('git config --get remote.origin.url', shell=True).replace('\n', '')
+                assert remote_url==github_url, "This repository already has a remote url: {}, which doesn't match the github url you listed: {}".format(remote_url, github_url)
+            else:
+                # From: https://stackoverflow.com/a/16811212/851699
+                subprocess.call('git init .', shell=True)
+                subprocess.call('git remote add -t \* -f origin {}'.format(github_url), shell=True)
+                subprocess.call('git checkout master', shell=True)
+
+            if not os.path.exists(readme_file):
+                with current_directory(destination_dir):
+                    repo_name = subprocess.check_output('basename `git rev-parse --show-toplevel`', shell=True).replace('\n', '')
+                    with open(readme_file, 'w') as f:
+                        f.write(readme_template.format(name=package_name, git_url=github_url, repo_name=repo_name, ))
+
+            if commit_and_push:
+                subprocess.call('git add .', shell=True)
+                subprocess.call('git commit -m "added initial files"', shell=True)
+                subprocess.call('git push -u origin master', shell=True)
+
+
+def get_github_url(user_or_org, repo_name):
+    return 'https://github.com/{user}/{name}.git'.format(user=user_or_org, name=repo_name)
+
+
+def get_pip_install_path(user_or_org, repo_name):
+    return 'git+http://github.com/{user_or_org}/{repo}.git#egg={repo}'.format(user_or_org=user_or_org, repo=repo_name)
+
+
+def create_github_repo(user, repo_name, private=False, org_name=None, if_existing='ok'):
+
+    assert private in (True, False)
+    assert if_existing in ('ok', 'check', 'error')
+    if org_name is None:
+        org_name=user
+
+    # Check if it already exists
+    github_url = get_github_url(user_or_org=org_name, repo_name=repo_name)
+    api_path = 'https://api.github.com/user/repos' if user==org_name else 'https://api.github.com/orgs/{org}/repos'.format(org=org_name)
+    repo_exists = "Not Found" not in subprocess.check_output('curl {api_path}'.format(api_path=api_path), shell=True)
+    # From: https://stackoverflow.com/a/23916276/851699
+
+    if repo_exists:
+        if if_existing=='ok':
+            pass
+        elif if_existing=='check':
+            print
+            import time; time.sleep(0.01)
+            response = raw_input('Repo {} already exists.  Proceed anyway with existing repo?  (y, n) >>'.format(github_url))
+            assert response in ('y', 'n')
+            if response!='y':
+                raise Exception('Repo {} already exists'.format(github_url))
+        elif if_existing=='error':
+            raise Exception('Repo {} already exists'.format(github_url))
+    else:
+        # From https://stackoverflow.com/a/10325316/851699
+        subprocess.call('curl -u \'{user}\' {api_path} -d \'{{"name":"{repo}", "private":"{private}"}}\''.format(user=user, api_path=api_path, repo=repo_name, private='true' if private else 'false'), shell=True)
+    return github_url
 
 
 def copy_and_create_setup(
--- public_release/ui.py
+++ public_release/ui.py
@@ -1,13 +1,14 @@
 import getpass
 import os
-
-from public_release.create_repo import fill_repo_with_initial_files, create_git_repo
+from ast import parse
+from public_release.create_repo import fill_repo_with_initial_files, get_github_url, create_github_repo, \
+    get_pip_install_path
 
 
 def get_user_response(prompt, default = None):
     if default is not None:
         prompt += ' [{}]'.format(default)
-    prompt += '>>'
+    prompt += ' >>'
     user_input = raw_input(prompt)
     stripped = user_input.lstrip(' ').rstrip(' ')
     if stripped=='':
@@ -19,30 +20,44 @@ def get_user_response(prompt, default = None):
         return stripped
 
 
+def is_valid_variable_name(name):
+    # Thank you mhawke: https://stackoverflow.com/a/36331242/851699
+    try:
+        parse('{} = None'.format(name))
+        return True
+    except (SyntaxError, ValueError, TypeError):
+        return False
+
+
 def ui_initialize_repo():
 
     print 'This will take you through initializing a repository.  Options in [square brackets] are defaults.'
 
+    # Github setup questions
     name = get_user_response('Enter Repo Name')
     assert ' ' not in name
+    github_user = get_user_response('Enter GitHub username')
+    github_org = get_user_response('Enter GitHub user/organization to create repo in', default=github_user)
+    is_private = get_user_response('Make Repo Private? (y/n)', default='n')
+    assert is_private in ('y', 'n')
 
     root_name = get_user_response('Enter Name of root code package', default=name.replace('-', '_'))
-    assert ' ' not in root_name
-
+    assert is_valid_variable_name(root_name)
     local_path = get_user_response('Enter Local Path for repo', default=os.path.join(os.path.expanduser("~"), 'projects', name))
     author = get_user_response('Author', default=getpass.getuser())
     dependencies = [d.lstrip(' ').rstrip(' ') for d in get_user_response('List Comma-Separated Dependencies (eg "numpy,scipy")', default='').split(',')]
 
-    git_user = get_user_response('Enter GitHub username/organization: ')
+    is_private = is_private=='y'
 
-    git_url = 'https://github.com/{user}/{name}.git'.format(user=git_user, name=name)
+    git_url = get_github_url(user_or_org=github_org, repo_name=name)
 
     response = get_user_response('Ready to create the following repo:\n' +
+         '  git url: {}\n'.format(git_url) +
          '  Root Package Name: {}\n'.format(root_name) +
          '  Local Path: {}\n'.format(local_path) +
          '  Author: {}\n'.format(author) +
          '  Dependencies: {}\n'.format(dependencies) +
-         '  git url: {}\n'.format(git_url) +
+         '  Private?: {}\n'.format('Yes' if is_private else 'No'),
          'Confirm Decision? (y/n)'
          )
 
@@ -50,7 +65,13 @@ def ui_initialize_repo():
         print 'Cancelled.'
         return
 
-    create_git_repo(local_folder=local_path, repo_url=git_url)
+    git_url = create_github_repo(
+        user=github_user,
+        repo_name=name,
+        private=is_private,
+        org_name=github_org,
+        if_existing='check'
+        )
 
     fill_repo_with_initial_files(
         package_name=name,
@@ -58,10 +79,17 @@ def ui_initialize_repo():
         root_package=root_name,
         author=author,
         install_requires=dependencies,
+        github_url=git_url,
         commit_and_push=True,
         )
 
-    print 'Created the repo: <a href="{url}">{url}</a>'.format(url=git_url)
+    print '='*15 + ' SUCCESS! ' + '='*15
+    print 'Created the repo: {}'.format(git_url)
+    print 'It exists locally at {}'.format(local_path)
+    print 'You can set up the repo with: \n  $ cd {}; source setup.sh'.format(local_path)
+    print 'Our you can install the repo as source in your current environment with: \n  $ pip install -e {}'.format(get_pip_install_path(user_or_org=github_org, repo_name=name))
+    print 'A new user can install your repo with: \n  $ git clone {}; cd {}; source setup.sh'.format(git_url, name)
+    print '='*40
 
 if __name__ == '__main__':
     ui_initialize_repo()
